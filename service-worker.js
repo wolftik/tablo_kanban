27c5e51b3d2a3ae811560eae66d2a94332c0f84a
akimenkoin <@kiko22061990>
2026-05-05 11:02:01 +0300

    фикс манифеста из за науршений перехвата

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,5 @@
 const NEWTAB = chrome.runtime.getURL('views/newtab.html');
 
-function isNewTab(url) {
-  return !url || url === 'chrome://newtab/' || url === 'about:blank' || url === 'about:newtab' || url === 'edge://newtab/';
-}
-
-// For browsers without chrome_url_overrides support (Yandex, Edge)
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'loading' && isNewTab(tab.url)) {
-    chrome.tabs.update(tabId, { url: NEWTAB }).catch(() => {});
-  }
-});
-
 // Open on extension icon click
 chrome.action.onClicked.addListener((tab) => {
   chrome.tabs.create({ url: NEWTAB });
